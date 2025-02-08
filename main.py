5b55383fc9e9af753bf764c9b3ab5442c5895deb
ReyhanSolaiman <solaimanreyhan@gmail.com>
2025-02-08 04:09:47 -0500

    D: add groups to manage obj

--- main.py
+++ main.py
@@ -7,14 +7,20 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        for entity in drawable:
+            entity.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
